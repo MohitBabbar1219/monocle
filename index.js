99c0014a683b7ba202c856bc6d903d233ae03b4b
Mohit Babbar <mohitbabbar1219@gmail.com>
2019-04-20 19:51:46 +0530

    Add body parser

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+const users = require('./routes/users');
+app.use('/api/users', users);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
