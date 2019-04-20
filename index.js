b2bc387a2afe1311e7f94281f12359bfec637aeb
Mohit Babbar <mohitbabbar1219@gmail.com>
2019-04-20 20:05:55 +0530

    Add connection to mongodb

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -6,7 +7,14 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+const db = require('./config/keys').mongoURI;
+
 const users = require('./routes/users');
+
+mongoose.connect(db)
+    .then(() => console.log('successfully connected to database...'))
+    .catch((err) => console.log('error occurred while connecting to database', err));
+
 app.use('/api/users', users);
 
 const PORT = process.env.PORT || 5000;
